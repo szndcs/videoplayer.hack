0540103682898af73dc63dd879ef3deb13db466b
szndcs <hello@szondicsaba.hu>
2022-08-30 12:12:08 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # videoplayer.hack
+A little hack to style differently the html5 videoplayer with pure js and css
+
+The process took about 5 - 5.5 hours. During this time:
+- to read the documentation of html5 videoplayer's controls took 1 hour,
+- to create the script from plan to completed took approx. 2 hours,
+- to search for icons, create mask file took approx. .5 hour,
+- to style the player took approx 1.5 hours,
+- to beautify and organizing the code took approx. .5 hour.
---
+++ assets/js/app.js
@@ -0,0 +1,60 @@
+window.onload = (event) => {
+    // let's initialize the video controls and contents after page load
+    const video = document.getElementById('video');
+    const playPause = document.getElementById('play-pause-button');
+    const currentTime = document.getElementById('current-time');
+    const mute = document.getElementById('mute-button');
+    video.controls = false;
+    playPause.innerHTML = '<i class="fa fa-play-circle"></i>';
+    mute.innerHTML = '<div class="eq"><div class="eq-line"></div><div class="eq-line"></div><div class="eq-line"></div><div class="eq-line"></div><div class="eq-line"></div><div class="eq-line"></div></div>';
+
+    // add lsiteners to controllers
+    video.addEventListener('timeupdate', (e)=>{
+        currentTime.innerHTML = video.currentTime.toMMSS();
+        eqAnim();
+    });
+
+    playPause.addEventListener('click', (e)=>{
+        if(video.paused || video.ended){
+            video.play();
+            playPause.innerHTML = '<i class="fa fa-pause-circle"></i>';
+        } else {
+            video.pause();
+            playPause.innerHTML = '<i class="fa fa-play-circle"></i>';
+        }
+    });
+
+    mute.addEventListener('click', (e)=>{
+        video.muted = !video.muted;
+    });
+
+};
+
+// create a prototype to convert video timestamp into mm:ss format
+Number.prototype.toMMSS = function(){
+    const timestamp = this;
+    let hh = Math.floor(timestamp / 3600);
+    let mm = Math.floor((timestamp - (hh * 3600)) / 60);
+    let ss = Math.floor(timestamp - (hh * 3600) - (mm * 60));
+    hh = (hh < 10) ? '0' + hh : hh;
+    mm = (mm < 10) ? '0' + mm : mm;
+    ss = (ss < 10) ? '0' + ss : ss;
+    return mm + ':' + ss;
+}
+
+// add fake equalizer effect to mute / unmute button
+function eqAnim() {
+    let brickNum = Math.floor(Math.random() * 5) + 1;
+    let heigth = Math.floor(Math.random() * 30) + 1.5;
+    let brick = document.querySelector('.eq :nth-child('+brickNum+')');
+    let allBricks = document.querySelectorAll('.eq-line');
+    if(!video.muted){
+        setTimeout(()=>{
+            brick.style.height = heigth+'px';
+        }, 1);
+    } else {
+        allBricks.forEach((item)=>{
+            item.style.height = '2px';
+        })
+    }
+}
